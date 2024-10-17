976045d7f187154f3d9ef99b03d16c14a8b3511f
AndriiCar <kravchenko.svg@gmail.com>
2024-10-17 16:50:29 +0300

    test

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -8,62 +8,66 @@ export const config: Options.Testrunner = {
 
     before: async function (capabilities, specs) {
         await browser.url('/');
-        await browser.maximizeWindow(); 
+        await browser.maximizeWindow();
 
         require('dotenv').config();
     },
 
-    specs: [
-        './test/specs/**/*.ts'
-    ],
-  
-    exclude: [
-      
-    ],
- 
+    specs: ['./test/specs/**/*.ts'],
+
+    exclude: [],
+
     maxInstances: 10,
 
-    capabilities: [{
-        browserName: 'chrome', 
-        'goog:chromeOptions': {
-            args: ['headless', 'disable-gpu'],
+    capabilities: [
+        {
+            browserName: 'chrome',
+            'goog:chromeOptions': {
+                args: ['headless', 'disable-gpu'],
+            },
+            acceptInsecureCerts: true,
         },
-        acceptInsecureCerts: true,
-    }],
+    ],
 
     logLevel: 'info',
 
     bail: 0,
-    
+
     baseUrl: process.env.BASE_URL,
     waitforTimeout: 10000,
 
     connectionRetryTimeout: 60000,
- 
+
     connectionRetryCount: 3,
-    
+
     framework: 'mocha',
-    
+
     mochaOpts: {
         ui: 'bdd',
-        timeout: 150000 
+        timeout: 150000,
     },
 
     reporters: [
         'spec',
-        ['mochawesome', {
-            outputDir: './mochawesome-reports',  
-            mochawesomeOpts: {
-                reportDir: 'mochawesome-report',
-                reportFilename: 'wdio-mochawesome', 
-                html: true,  
-                json: true   
-            }
-        }],
-        ['allure', {
-            outputDir: 'allure-results',
-            disableWebdriverStepsReporting: true,
-            disableWebdriverScreenshotsReporting: false,
-        }]
+        [
+            'mochawesome',
+            {
+                outputDir: './mochawesome-reports',
+                mochawesomeOpts: {
+                    reportDir: 'mochawesome-report',
+                    reportFilename: 'wdio-mochawesome',
+                    html: true,
+                    json: true,
+                },
+            },
+        ],
+        [
+            'allure',
+            {
+                outputDir: 'allure-results',
+                disableWebdriverStepsReporting: true,
+                disableWebdriverScreenshotsReporting: false,
+            },
+        ],
     ],
-}
+};
